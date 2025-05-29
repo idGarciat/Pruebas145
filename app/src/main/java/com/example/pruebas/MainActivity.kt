c0fc450bd595e3f7555b0b37116e8f40aa6c1ae8
bspoloo <animetx098@gmail.com>
2025-05-29 16:12:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pruebas/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.pruebas
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //Declarando variables con los botones
+        val botondesalir = findViewById<Button>(R.id.botonsalir)
+        val botondeinicio = findViewById<Button>(R.id.botoninicio)
+
+        //Funcion de los botones
+        botondesalir.setOnClickListener {
+            botondesalir.text = "Adios"
+            finish()
+        }
+
+        botondeinicio.setOnClickListener {
+            //Ventana emergente al hacer click en el boton
+
+
+        }
+
+
+
+    }
+}
